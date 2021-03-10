1c0ae50320956ee60d3a63d7653bd11d015e7ef4
an-geon <gunan0407@naver.com>
2021-03-10 22:17:53 +0900

    removed helloworld.java added MenuManager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,11 @@
+
+public class MenuManager {
+	public static void main(String[] args) {
+		System.out.println("1. Add PocketMoney");
+		System.out.println("2. Delete PocketMoney");
+		System.out.println("3. Edit PocketMoney");
+		System.out.println("4. View PocketMoney");
+		System.out.println("5. Exit ");
+
+	}
+}
